44241218dcc3ed2f7887b39398b4d2d204a57422
changyoung4087 <double4087@naver.com>
2023-06-23 11:36:12 +0900

    소스정리

--- src/main/java/org/warehouse/controllers/users/UserInfoService.java
+++ src/main/java/org/warehouse/controllers/users/UserInfoService.java
@@ -7,13 +7,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
-import org.warehouse.configs.models.mapper.UserMapper;
-import org.warehouse.models.user.User;
-=======
 import org.warehouse.configs.models.mapper.UserDAO;
 import org.warehouse.models.user.UserVO;
->>>>>>> 0bb7b9657971b9bf3f3ac1a2255b34ce7ecc6707
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,35 +16,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserInfoService implements UserDetailsService {
-<<<<<<< HEAD
-	private final UserMapper userMapper;
-=======
 	private final UserDAO userDAO;
->>>>>>> 0bb7b9657971b9bf3f3ac1a2255b34ce7ecc6707
 
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-<<<<<<< HEAD
-		User user = userMapper.getUserById(username);
-		if(user == null) {
-			throw new UsernameNotFoundException(username);
-		}
-
-		List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority(user.getUserType()));
-
-		return UserInfo.builder()
-				.userId(user.getUserId())
-				.userPw(user.getUserPw())
-				.userNm(user.getUserNm())
-				.userType(user.getUserType())
-				.clntCd(user.getClntCd())
-				.custCtrCd(user.getCustCtrCd())
-				.custCd(user.getCustCd())
-				.position(user.getPosition())
-				.tel(user.getTel())
-				.email(user.getEmail())
-=======
 		UserVO userVO = userDAO.getUserById(username);
 		System.out.println(userVO);
 		if(userVO == null) {
@@ -69,7 +40,6 @@ public class UserInfoService implements UserDetailsService {
 				.position(userVO.getPosition())
 				.tel(userVO.getTel())
 				.email(userVO.getEmail())
->>>>>>> 0bb7b9657971b9bf3f3ac1a2255b34ce7ecc6707
 				.authorities(authorities)
 				.build();
 	}
--- src/main/java/org/warehouse/services/UserService.java
+++ src/main/java/org/warehouse/services/UserService.java
@@ -3,39 +3,14 @@ package org.warehouse.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
-import org.warehouse.configs.models.mapper.UserMapper;
-import org.warehouse.models.user.User;
-=======
 import org.warehouse.configs.models.mapper.UserDAO;
 import org.warehouse.models.user.UserVO;
->>>>>>> 0bb7b9657971b9bf3f3ac1a2255b34ce7ecc6707
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-<<<<<<< HEAD
-	private final UserMapper userMapper;
-	private final PasswordEncoder passwordEncoder;
-
-	public List<User> getUserList() {
-		return userMapper.getUserList();
-	}
-
-	public User getUserById(String userId) {
-		return userMapper.getUserById(userId);
-	}
-
-	public User getUserByEmail(String email) {
-		return userMapper.getUserByEmail(email);
-	}
-
-	public void register(User user) {
-		user.setUserPw(passwordEncoder.encode(user.getUserPw()));
-		userMapper.insertUser(user);
-=======
 	private final UserDAO userDAO;
 	private final PasswordEncoder passwordEncoder;
 
@@ -52,7 +27,6 @@ public class UserService {
 
 	public UserVO getUserByEmail(String email) {
 		return userDAO.getUserByEmail(email);
->>>>>>> 0bb7b9657971b9bf3f3ac1a2255b34ce7ecc6707
 	}
 
 }
