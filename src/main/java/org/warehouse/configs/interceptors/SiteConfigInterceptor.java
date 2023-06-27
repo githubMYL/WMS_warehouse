08d3f2c4d689388f517c23985e7cdd8b7a6a3805
changyoung4087 <double4087@naver.com>
2023-06-27 13:14:47 +0900

    git 중복오류 수정

--- src/main/java/org/warehouse/configs/interceptors/SiteConfigInterceptor.java
+++ src/main/java/org/warehouse/configs/interceptors/SiteConfigInterceptor.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.warehouse.configs.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,20 +13,3 @@ public class SiteConfigInterceptor implements HandlerInterceptor {
 		return true;
 	}
 }
-=======
-package org.warehouse.configs.interceptors;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-@Component
-public class SiteConfigInterceptor implements HandlerInterceptor {
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		request.setAttribute("cssJsVersion", 1);
-		return true;
-	}
-}
->>>>>>> 28b5aa5266629fa9c8358416a99e8709c1b7bf6d
