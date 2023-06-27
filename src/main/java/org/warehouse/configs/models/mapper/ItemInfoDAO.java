78f18a4cfe80ddb2f3688485d78a5436fd95f164
lww9562 <lww9562@naver.com>
2023-06-27 10:35:52 +0900

    소스 통합 - 20230627

---
+++ src/main/java/org/warehouse/configs/models/mapper/ItemInfoDAO.java
@@ -0,0 +1,15 @@
+package org.warehouse.configs.models.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.warehouse.models.baseinfo.iteminfo.ItemInfoVO;
+
+
+import java.util.List;
+
+@Mapper
+public interface ItemInfoDAO {
+
+    void insertItemInfo(ItemInfoVO itemInfoVO); // 상품정보 저장
+
+    List<ItemInfoVO> getCodeList(); // 관리단위 코드 가져오기
+}
