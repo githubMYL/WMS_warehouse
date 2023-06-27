28b5aa5266629fa9c8358416a99e8709c1b7bf6d
lww9562 <lww9562@naver.com>
2023-06-27 11:08:46 +0900

    소스 통합 - 20230627

---
+++ src/main/java/org/warehouse/models/baseinfo/iteminfo/ItemInfoService.java
@@ -0,0 +1,19 @@
+package org.warehouse.models.baseinfo.iteminfo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.warehouse.configs.models.mapper.ItemInfoDAO;
+
+@Service
+@RequiredArgsConstructor
+public class ItemInfoService {
+
+	private final ItemInfoDAO itemInfoDAO;
+
+	public void itemInfoSave(ItemInfoVO itemInfoVO){
+
+		System.out.println("service :: " + itemInfoVO);
+		itemInfoDAO.insertItemInfo(itemInfoVO);
+	}
+
+}
