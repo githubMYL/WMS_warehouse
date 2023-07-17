a47c10cf5e4ed05e4975d58816f231be290cf720
lww9562 <lww9562@naver.com>
2023-07-18 06:35:12 +0900

    재고이동 - 불량수량 추가

--- src/main/java/org/warehouse/controllers/tmstk/StockController.java
+++ src/main/java/org/warehouse/controllers/tmstk/StockController.java
@@ -191,6 +191,10 @@ public class StockController {
 	@GetMapping("_transfForm")
 	public String transfForm(@Param("locCd")String locCd, @Param("itemCd")String itemCd, @Param("wactrCd")String wactrCd, Model model) {
 		StktransfVO stktransfVO = new StktransfVO();
+		//기본값
+		stktransfVO.setMoveNormal(0L);
+		stktransfVO.setMoveFault(0L);
+
 
 		TmstkVO tmstkVO = stockDAO.getTmstkByLocCdItemCd(locCd, itemCd);
 		List<LocVO> locList = locDAO.getLocListByWactrCd(tmstkVO.getWactr_cd());
@@ -218,10 +222,13 @@ public class StockController {
 		stktransfVO.setRuntimeStock(tmstkVO.getStock_amt());	// 추후에 출고쪽 물려있는 수량 뺄 예정
 		stktransfVO.setRegNm(userInfo.getUserNm());
 
+		stktransfVO.setMoveAmt(stktransfVO.getMoveNormal()+stktransfVO.getMoveFault());		// 이동수량 = 정상이동수량 + 불량이동수량
+
 		// 시점재고의 값을 변경한다.
 		// 기존 로케이션에서 재고를 옮길 때, 기존 로케이션에 잔여 재고가 있는 경우
 		if(tmstkVO.getStock_amt() > stktransfVO.getMoveAmt()){
 			// 기존 로케이션의 재고 값을 수정한다.
+			tmstkVO.setFault_amt(tmstkVO.getFault_amt()-stktransfVO.getMoveFault());
 			tmstkVO.setStock_amt(tmstkVO.getStock_amt()-stktransfVO.getMoveAmt());
 			tmstkVO.setModNm(userInfo.getUserNm());
 			stockDAO.updateTmstkAmt(tmstkVO);
@@ -231,6 +238,7 @@ public class StockController {
 			if(stockDAO.getTmstkByLocCdItemCd(stktransfVO.getLocCdTo(), stktransfVO.getItemCd()) != null) {
 				TmstkVO existTmstk = stockDAO.getTmstkByLocCdItemCd(stktransfVO.getLocCdTo(), stktransfVO.getItemCd());
 				existTmstk.setStock_amt(existTmstk.getStock_amt() + stktransfVO.getMoveAmt());
+				existTmstk.setFault_amt(existTmstk.getFault_amt() + stktransfVO.getMoveFault());
 				existTmstk.setModNm(userInfo.getUserNm());
 
 				stockDAO.updateTmstkAmt(existTmstk);
@@ -243,7 +251,7 @@ public class StockController {
 				toTmstk.setLoc_cd(stktransfVO.getLocCdTo());		// 이동할 로케이션 코드
 				toTmstk.setWactr_cd(stktransfVO.getWaCtrCdTo());	// 물류센터 코드
 				toTmstk.setStock_amt(stktransfVO.getMoveAmt());		// 이동 수량
-				toTmstk.setFault_amt(0L);							// 불량을 이동하는 경우는 들어가있지 않았음
+				toTmstk.setFault_amt(stktransfVO.getMoveFault());	// 불량 수량
 
 				toTmstk.setRegNm(userInfo.getUserNm());
 
@@ -262,15 +270,15 @@ public class StockController {
 		else if(tmstkVO.getStock_amt() == stktransfVO.getMoveAmt()){
 			// 기존 로케이션의 가용 재고를 비우고, 만약 fault_amt 또한 0이라면, 테이블을 비운다.
 			tmstkVO.setStock_amt(0L);
-			if(tmstkVO.getFault_amt() == 0) {
-					stockDAO.deleteTmstk(tmstkVO);
-			}
+			tmstkVO.setFault_amt(0L);
+			stockDAO.deleteTmstk(tmstkVO);
 
 
 			// 만약, 옮겨갈 로케이션이 기존에 재고가 존재할 경우, 값을 추가하여 수정,
 			if(stockDAO.getTmstkByLocCdItemCd(stktransfVO.getLocCdTo(), stktransfVO.getItemCd()) != null) {
 				TmstkVO existTmstk = stockDAO.getTmstkByLocCdItemCd(stktransfVO.getLocCdTo(), stktransfVO.getItemCd());
 				existTmstk.setStock_amt(existTmstk.getStock_amt() + stktransfVO.getMoveAmt());
+				existTmstk.setFault_amt(existTmstk.getFault_amt() + stktransfVO.getMoveFault());
 				existTmstk.setModNm(userInfo.getUserNm());
 
 				stockDAO.updateTmstkAmt(existTmstk);
@@ -283,7 +291,7 @@ public class StockController {
 				toTmstk.setLoc_cd(stktransfVO.getLocCdTo());		// 이동할 로케이션 코드
 				toTmstk.setWactr_cd(stktransfVO.getWaCtrCdTo());	// 물류센터 코드
 				toTmstk.setStock_amt(stktransfVO.getMoveAmt());		// 이동 수량
-				toTmstk.setFault_amt(0L);							// 불량을 이동하는 경우는 들어가있지 않았음
+				toTmstk.setFault_amt(stktransfVO.getMoveFault());	// 불량 수량
 
 				toTmstk.setRegNm(userInfo.getUserNm());
 
